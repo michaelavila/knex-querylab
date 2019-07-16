35d8e4b6ac4d3d87fa4720f068d66cc214a71c45
Michael Avila <davidmichaelavila@gmail.com>
2019-07-15 23:40:42 -0700

    Add very simple translator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Knex from 'knex';
 import './App.css';
 
+function translate(knexjs: string): string {
+  const knex = Knex({client: 'postgres'})
+  try {
+    return eval(knexjs).toQuery();
+  } catch {
+    return "syntax error"
+  }
+}
+
 const App: React.FC = () => {
+  const [query, setQuery] = useState("knex('table');")
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <textarea onChange={(e) => setQuery(e.target.value || "")} value={query}></textarea>
+      <p>{translate(query)}</p>
+    </div>
   );
 }
 
