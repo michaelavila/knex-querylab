00b290693164e380496cef2bfb272a5427aac813
Michael Avila <mavila@carbonfive.com>
2021-03-17 18:06:09 -0700

    syntax highlighting for editing js query and viewing sql query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React, { useCallback, ChangeEvent } from 'react';
-
-import LZString from 'lz-string';
-
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
 import './App.css';
+
+// querylab lib code
 import { Dialect, allDialects, translate } from './querylab';
+
+// used for LZ compress query and store in query string
+import LZString from 'lz-string';
 import { useQueryString } from './use-query-string';
+
+//
+// code editor and syntax highlighter
+//
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs';
+import 'prismjs/components/prism-sql';
+import 'prismjs/themes/prism.css'; // change the theme here
+
 
 // decompresses to: knex('change').select('me').count();
 const DEFAULT_QUERY = "NYOwpgHgFA5AxgCwIYgOZhgSgHQGcwA2YcALrALYY5wD2AriGZgNxA";
-
 const DEFAULT_DIALECT = Dialect.postgres;
 
 export default function App() {
@@ -22,8 +29,8 @@ export default function App() {
 		setDialect(e.target.value);
 	}, [setDialect]);
 
-	const updateQuery = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
-		const compressed = LZString.compressToEncodedURIComponent(e.target.value);
+	const updateQuery = useCallback((value: string) => {
+		const compressed = LZString.compressToEncodedURIComponent(value);
 		setQuery(compressed);
 	}, [setQuery]);
 
@@ -43,10 +50,21 @@ export default function App() {
 			</div>
 
 			{/* body - input knex query*/}
-			<div><textarea onChange={updateQuery} value={displayQuery}></textarea></div>
+			<div>
+				<Editor
+					value={displayQuery}
+					onValueChange={updateQuery}
+					highlight={(code) => highlight(code, languages.javascript, 'javascript')} />
+			</div>
 
 			{/* body - output sql*/}
-			<div><code>{translate(displayQuery, dialect)}</code></div>
+			<div>
+				<Editor
+					disabled={true}
+					value={translate(displayQuery, dialect)}
+					onValueChange={(value) => console.log(value)}
+					highlight={(code) => highlight(code, languages.sql, 'sql')} />
+			</div>
 
 			{/* footer */}
 			<div>
