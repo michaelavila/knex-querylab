2e0e95305b664019e10eb8fd51eae1e81389e545
Michael Avila <mavila@carbonfive.com>
2021-03-18 22:17:22 -0700

    small style tweak

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,9 @@ import 'prismjs/themes/prism-solarizedlight.css'; // change the theme here
 
 import { format } from 'sql-formatter';
 
-import { FormControl, InputLabel, Select, MenuItem, Typography, Table, TableHead, TableBody, TableRow, TableCell, Link, Paper, Grid, TableContainer, Snackbar, Button } from '@material-ui/core';
+import { AppBar, Toolbar, FormControl, InputLabel, Select, MenuItem, Typography, Table, TableHead, TableBody, TableRow, TableCell, Link, Paper, Grid, TableContainer, Snackbar, Button, Menu } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
+import { createStyles, makeStyles, Theme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 // decompresses to: knex('change').select('me').count();
 const DEFAULT_QUERY = "NYOwpgHgFA5AxgCwIYgOZhgSgHQGcwA2YcALrALYY5wD2AriGZgNxA";
@@ -28,6 +29,30 @@ const DEFAULT_DIALECT = Dialect.postgres;
 const FORMAT_OPTIONS = {
 	uppercase: true,
 };
+
+export const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#121113',
+		},
+		secondary: {
+			main: '#f0f0f2',
+		},
+	},
+	overrides: {
+		MuiSelect: {
+			icon: {
+				color: "#f0f0f2",
+			},
+			select: {
+				"&:focus": {
+					background: "#3a3660",
+				},
+				color: "#f0f0f2",
+			},
+		},
+	},
+});
 
 export default function App() {
 	const [dialect, setDialect] = useQueryString('dialect', DEFAULT_DIALECT);
@@ -58,86 +83,88 @@ export default function App() {
 	}, [setMessage]);
 
 	return (
-		<div className='App'>
-			<Snackbar open={message !== null} autoHideDuration={3000} onClose={(e) => clearMessage()} >
-				<Alert severity="success" onClose={(e) => clearMessage()}>
-					Copied to clipboard!
-				</Alert>
-			</Snackbar>
+		<ThemeProvider theme={theme}>
 
-			{/* header */}
-			<header><Typography variant='h2'>Knex QueryLab</Typography></header>
+			<div className='App'>
+				{/* header */}
+				<AppBar position="static">
+					<Toolbar>
+						<Typography variant="h6">
+							Knex QueryLab
+						</Typography>
+						<div>
+							<Select variant="outlined" labelId="dialect-label" value={dialect} onChange={(e) => updateDialect(e.target.value as string)} label="Dialect">
+								{allDialects().map((d, i) => <MenuItem key={i} value={d}>{d}</MenuItem>)}
+							</Select>
+						</div>
+					</Toolbar>
+				</AppBar>
 
-			<section>
-				{/* body */}
+				<section>
+					{/* body */}
 
-				{/* body - select dialect */}
-				<FormControl variant="outlined">
-					<InputLabel id="dialect-select-label">Dialect</InputLabel>
-					<Select className='dialect' labelId="dialect-select-label" value={dialect} onChange={(e) => updateDialect(e.target.value as string)} label="Dialect">
-						{allDialects().map((d, i) => <MenuItem key={i} value={d}>{d}</MenuItem>)}
-					</Select>
-				</FormControl>
+					{/* body - select dialect */}
 
-				<Typography className='subtitle' variant='h6'>Expression</Typography>
+					<Typography className='subtitle' variant='h6'>Expression</Typography>
 
-				<Button style={{float: 'right'}}	onClick={() => copy(displayQuery)}>Copy</Button>
-				{/* body - input knex query*/}
-				<Paper className='code'>
-					<Editor
-						value={displayQuery}
-						onValueChange={updateQuery}
-						highlight={(code) => highlight(code, languages.javascript, 'javascript')} />
-				</Paper>
+					<Button style={{float: 'right'}}	onClick={() => copy(displayQuery)}>Copy</Button>
+					{/* body - input knex query*/}
+					<Paper className='code'>
+						<Editor
+							value={displayQuery}
+							onValueChange={updateQuery}
+							highlight={(code) => highlight(code, languages.javascript, 'javascript')} />
+					</Paper>
 
-				<Typography className='subtitle' variant='h6'>Query</Typography>
+					<Typography className='subtitle' variant='h6'>Query</Typography>
 
-				<Button style={{float: 'right'}}	onClick={() => copy(toQuery)}>Copy</Button>
-				{/* body - output sql*/}
-				<Paper className='code'>
-					<Editor
-						disabled={true}
-						value={toQuery}
-						onValueChange={noop}
-						highlight={(code) => highlight(format(code, FORMAT_OPTIONS), languages.sql, 'sql')} />
-				</Paper>
+					<Button style={{float: 'right'}}	onClick={() => copy(toQuery)}>Copy</Button>
+					{/* body - output sql*/}
+					<Paper className='code'>
+						<Editor
+							disabled={true}
+							value={toQuery}
+							onValueChange={noop}
+							highlight={(code) => highlight(format(code, FORMAT_OPTIONS), languages.sql, 'sql')} />
+					</Paper>
 
-				<Typography className='subtitle' variant='h6'>SQL - Native</Typography>
+					<Typography className='subtitle' variant='h6'>SQL - Native</Typography>
 
-				{/* body - output native*/}
-				<Paper className='code'>
-					<Editor
-						disabled={true}
-						value={sql}
-						onValueChange={noop}
-						highlight={(code) => highlight(format(code, FORMAT_OPTIONS), languages.sql, 'sql')} />
-				</Paper>
+					{/* body - output native*/}
+					<Paper className='code'>
+						<Editor
+							disabled={true}
+							value={sql}
+							onValueChange={noop}
+							highlight={(code) => highlight(format(code, FORMAT_OPTIONS), languages.sql, 'sql')} />
+					</Paper>
 
-				{/* body - output bindings*/}
-				<TableContainer	component={Paper} className='bindings'>
-					<Table>
-						<TableHead>
-							<TableRow><TableCell style={{ width: '20px' }}>Binding</TableCell><TableCell>Value</TableCell></TableRow>
-						</TableHead>
-						<TableBody>
-							{bindings.map((value, index) => {
-								return (<TableRow key={index+1}>
-									<TableCell>{index+1}</TableCell>
-									<TableCell>{JSON.stringify(value)}</TableCell>
-								</TableRow>);
-							})}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</section>
+					{/* body - output bindings*/}
+					<TableContainer	component={Paper} className='bindings'>
+						<Table>
+							<TableHead>
+								<TableRow><TableCell style={{ width: '20px' }}>Binding</TableCell><TableCell>Value</TableCell></TableRow>
+							</TableHead>
+							<TableBody>
+								{bindings.map((value, index) => {
+									return (<TableRow key={index+1}>
+										<TableCell>{index+1}</TableCell>
+										<TableCell>{JSON.stringify(value)}</TableCell>
+									</TableRow>);
+								})}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</section>
 
-			{/* footer */}
-			<footer>
-				<Typography variant='body1'>
-					Experiment with the <Link href='https://knexjs.org'>KnexJS</Link> API to build
-					SQL. <Link href="https://github.com/michaelavila/knex-querylab">View source.</Link>
-				</Typography>
-			</footer>
-		</div>
+				{/* footer */}
+				<footer>
+					<Typography variant='body1'>
+						Experiment with the <Link href='https://knexjs.org'>KnexJS</Link> API to build
+						SQL. <Link href="https://github.com/michaelavila/knex-querylab">View source.</Link>
+					</Typography>
+				</footer>
+			</div>
+		</ThemeProvider>
 	);
 };
